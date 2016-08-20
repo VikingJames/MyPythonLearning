6d3bf2ea84d1f732fe1ff191b762536c7156f9ee
zhangbo <zb1989616@126.com>
2016-08-20 23:59:41 +0800

    第一次更新

---
+++ 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#1-2
+print "Hello world!"
+#1-4
+print 2+2
+print 53672+235253
+print 1.0/2.0
+print 1/2.0
+print 1/2.
+print 1/2
+print 1//2
+print 1.0 // 2.0
+print 1%2
+print 10%3
