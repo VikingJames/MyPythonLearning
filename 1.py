da10d2c7bf044ce8e43a29c5587dc8241f6cc5ef
zhangbo <zb1989616@126.com>
2016-08-21 23:59:05 +0800

    更新第一章的学习记录

--- 1.py
+++ 1.py
@@ -1,15 +1,48 @@
 #!/usr/bin/python
-
+#coding:utf-8
 #1-2
+print "#1-2"
 print "Hello world!"
 #1-4
+print "#1-4"
 print 2+2
 print 53672+235253
 print 1.0/2.0
 print 1/2.0
 print 1/2.
 print 1/2
-print 1//2
+print 1//2#整除
 print 1.0 // 2.0
 print 1%2
 print 10%3
+print 9/3
+print 9%3
+print 2**3#幂指數
+print -3**2
+print 10000000000000000000
+print 0xaf
+print 010
+#1-5
+print "#1-5"
+x = 3
+print x*2
+#1-7
+print "#1-7"
+#input("The meaning of life:")
+#1-8
+print "#1-8"
+print pow(2,3)
+print 10*pow(2,3*5)/3.0
+print abs(-10)
+print round(1.0/2.0)
+#1-9
+print "#1-9"
+import math
+print int(math.floor(32.9))
+import cmath
+print cmath.sqrt(-1)
+print (1+3j)*(9+4j)
+#1-10
+print "#1-10"
+#name = raw_input("What is your name:")
+#print "Hello " + name
