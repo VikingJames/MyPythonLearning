5ddf6112f8e18d7bc5a101f1e072a8b4f4988ae1
zhangbo2 <zhangbo2@pset.suntec.net>
2016-08-22 18:41:41 +0800

    开始第二章学习、
    Change-Id: I062bdcdfbed85cbeb0af931fe5a172e7e5e13e2e

--- 2.py
+++ 2.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python
+#coding:utf-8
+#2-1
+print "#2-1"
+edward = ['Edward Gumby', 42]
+print edward
+john = ['John Smith', 50]
+database = [edward, john]
+print database
+#2-2
+print "#2-2"
+greeting = 'Hello'
+print greeting[0]
+print greeting[-1]#若索引为负数，则反向计数
+print 'Hello'[1]
+fourth = raw_input('Year: ')[3]
+print fourth
+months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December']
