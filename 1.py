823a91bbdd145e935c5603fea0477595416c5522
zhangbo2 <zhangbo2@pset.suntec.net>
2016-08-22 11:55:18 +0800

    第一章学习结束
    Change-Id: If72854f25039b3e4ca59e6ed617a4b0b76a28b43

--- 1.py
+++ 1.py
@@ -46,3 +46,20 @@ print (1+3j)*(9+4j)
 print "#1-10"
 #name = raw_input("What is your name:")
 #print "Hello " + name
+#1-11
+print "#1-11"
+print 'Let\'s go!'
+print "Hello " + "world"
+print repr("Hello world")
+print repr(10000L)
+print str("Hello world")
+temp = 42
+print "The temperature is " + `temp`
+print str("100000L")
+#name = raw_input("What is your name: ")
+#print "My name is : " + name
+print '''Hello ,
+	world'''
+print r'c:\nopath'#原始字符串中不需要\\，除非需要转义
+print r'c:\program''\\'#原始字符串的结尾不能时\，可以使用前面的方式来在结尾打印\
+print u'中国'#unicode编码前加u
