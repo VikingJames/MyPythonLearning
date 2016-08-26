ec8411d3076e308571ce1197690a80d05c497dfb
zhangbo <zb1989616@126.com>
2016-08-26 23:37:20 +0800

    更新第五章的学习

--- 5.py
+++ 5.py
@@ -49,3 +49,68 @@ x = y = [1,2,3]
 z = [1,2,3]
 print x is y
 print x is z#此处为false是因为x和z是指向两个内容相同的不同对象
+
+x = [1,2,3]
+y = [2,4]
+print x is not y
+del x[2]
+y[1] = 1
+y.reverse()
+print x == y
+print x is y
+
+print 'alpha' < 'beta'
+
+print 'INTER'.lower() == 'inteR'.lower()
+
+#number = input('Enter a number between 1 and 10: ')
+#if number >= 1 and number <= 10:
+#	print 'Great'
+#else:
+#	print 'Wrong'
+
+age = 10
+assert 0 < age < 100
+#age = -1
+#assert 0 < age <100
+
+x = 1
+while x <= 100:
+	print x
+	x += 1
+
+#name = ''
+#while not name.strip():
+#	name = raw_input('Please input your name:')
+#print 'Your name is %s' % name
+
+print '\n'
+words = ['this','is','an','ex','parrot']
+for word in words:
+	print word
+
+print range(1,10)
+
+d = {'x':1,'y':2,'z':3}
+for key,value in d.items():
+	print key, value
+
+names = ['anne','beth','george','damon']
+ages = [12,45,32,102]
+for i in range(len(names)):
+	print names[i],'is',ages[i],'years old'
+
+for name ,age in zip(names,ages):
+	print name ,'is',age,'years old'
+
+from math import sqrt
+for n in range(99,0,-1):
+	root = sqrt(n)
+	if root == int(root):
+		print root
+		break
+
+while True:
+	word = raw_input('Please enter a word: ')
+	if not word:break
+	print 'The word is %s' % word
